232bd897c4eae31c997c40e0f40dad945feecd68
Yunchoel Lee <yun@Yunchoels-MacBook-Air.local>
2021-12-22 04:24:16 -0800

    make sure where do I want to recursion

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
-function sumItems(array) {
-  // Sum all the numbers in the array
-}
+const sumItems = function(array) {
+  let result = 0;
+  for (let i of array) {
+    console.log(i)
+    if (Array.isArray(i)) {
+      result += sumItems(i);
+    } else {
+      result += i;
+      console.log("resuljt",result)
+    }
+  }
+  return result;
+};
+console.log(sumItems([[1, 2, [[3], 4]], 5, []])); //15
 
 module.exports = sumItems;
+
